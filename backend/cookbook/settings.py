d3254ea95c064b1b1b1338251b7023c0eec4d38f
usagicake <kira_pan3@mail.ru>
2026-03-06 23:51:10 +0300

    23 50

--- backend/cookbook/settings.py
+++ backend/cookbook/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'api',
     'users',
     'recipes',
     'achievements',
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://localhost:8000/api/auth/login/', {
+        const response = await fetch('http://localhost:8000/api/auth/users/login/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
