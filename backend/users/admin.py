da2add60daa764dba5c7b4b683e34ff7b6151b96
Hatter1936 <dsolomko20@gmail.com>
2026-03-08 20:01:46 +0300

    Update Backend 1.4

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 from .models import User
 
-admin.site.register(User)
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'created_at')
+    search_fields = ('username',)
+    
+    def save_model(self, request, obj, form, change):
+        if 'password' in form.changed_data:
+            obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)
